4c6f362e4befff1463c9a3e9b035dbe89b300b21
Eder Franco <eder.franco@jcatlm.com.br>
2022-08-18 18:41:41 -0300

    Observing reducer and using it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addScheduling } from './agenda-state-store/agenda.actions';
+import { AgendaModel } from './models/agenda.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab-angular-redux';
+
+  constructor(
+    private store: Store<{agenda: AgendaModel}>
+  ) {}
+
+  async addScheduling(scheduling) {
+    console.log('AppComponent.addScheduling()');
+    this.store.dispatch(addScheduling(scheduling));
+  }
+
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { schedulingsCounter } from '../agenda-state-store/agenda.selectors';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  schedulingsCounter$: Observable<number>;
+
+  constructor(private store: Store) {
+    this.schedulingsCounter$ = store.select(schedulingsCounter);
+  }
 
   ngOnInit(): void {
   }
